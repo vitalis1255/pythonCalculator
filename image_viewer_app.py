1d7a6d4f57a8bf6edf41105306ea7d7806ea9a5c
vitalis1255 <vitalis.onyeke.175508@unn.edu.ng>
2026-09-08 13:31:57 +0100

    save the changes to status

--- image_viewer_app.py
+++ image_viewer_app.py
@@ -3,7 +3,7 @@ from PIL import ImageTk,Image
 
 root = Tk()#root window
 root.title("Image App")
-root.geometry("140x100")
+root.geometry("140x110")
 root.resizable(width=False, height=False)
 
 #Image one
@@ -33,6 +33,9 @@ my_img5 = ImageTk.PhotoImage(my_img5_resized)
 
 img_list = [my_img1, my_img2, my_img3, my_img4, my_img5]
 
+#bd=border,SUNKEN is used to make a widget pressed down
+status = Label(root, text="image 1 of " + str(len(img_list)), bd=2, relief=SUNKEN,padx=10, anchor=E)#anchor=position the label status to East.
+
 #First Image body show
 my_label = Label(image=my_img1)
 my_label.grid(row=0, column=0,columnspan=3)
@@ -41,6 +44,7 @@ def forward(image_number):
   global my_label
   global button_forward
   global button_back
+  #global status
 
   my_label.grid_forget()#makes images to disappear from the screen
   my_label = Label(image=img_list[image_number - 1])
@@ -50,9 +54,14 @@ def forward(image_number):
   if image_number == 5:
     button_forward = Button(root, text=">>", state=DISABLED)
 
+  #update the arrangement of the buttons and label in the forward function
   my_label.grid(row=0, column=0,columnspan=3)
   button_back.grid(row=1, column=0)
   button_forward.grid(row=1, column=2)
+
+  #update the status bar in the forward function
+  status = Label(root, text="image" + " " + str(image_number) + " " +  "of " + str(len(img_list)), bd=2, relief=SUNKEN,padx=10, anchor=E)#anchor=position the label status to East.
+  status.grid(row=2,column=0, columnspan=3,pady=3,sticky=(W + E))
 
   
 def back(image_number):
@@ -65,12 +74,18 @@ def back(image_number):
   button_forward = Button(root, text=">>", command=lambda:forward(image_number + 1))
   button_back = Button(root, text="<<", command=lambda: back(image_number - 1))
 
+   #Disabble if image_number reduced to 1
   if image_number == 1:
     button_back = Button(root, text="<<", state=DISABLED)
 
+  #update arragement of the buttons in the back function
   my_label.grid(row=0, column=0,columnspan=3)
   button_back.grid(row=1, column=0)
   button_forward.grid(row=1, column=2)
+
+  #update status bar in the back function
+  status = Label(root, text="image" + " " + str(image_number) +  " " +  "of " + str(len(img_list)), bd=2, relief=SUNKEN,padx=10, anchor=E)#anchor=position the label status to East.
+  status.grid(row=2,column=0, columnspan=3,pady=3,sticky=(W + E))
 
   
 #create button
@@ -82,6 +97,7 @@ button_forward = Button(root, text=">>", command=lambda: forward(2))
 button_back.grid(row=1, column=0)
 button_exit.grid(row=1, column=1)
 button_forward.grid(row=1, column=2)
+status.grid(row=2,column=0, columnspan=3,pady=3,sticky=(W + E))
 
 
 
