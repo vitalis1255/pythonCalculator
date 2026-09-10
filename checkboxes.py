f0a1341ea6f9a4a496fa597527c16be5a43b724c
vitalis1255 <vitalis.onyeke.175508@unn.edu.ng>
2026-09-10 12:55:53 +0100

    save checkbox.py to github

--- checkboxes.py
+++ checkboxes.py
@@ -6,9 +6,11 @@ root.title("Check boxes")
 root.geometry("300x300")
 root.resizable(width=False,height=False)
 
-var = IntVar()
+#var = IntVar()
+var = StringVar()#onvalue="on",offvalue="off" is added to this.
 
-c = Checkbutton(root, text="check this box, I dare you",variable=var)
+c = Checkbutton(root, text="check this box, I dare you",variable=var, onvalue="On",offvalue="Off")
+c.deselect()#do not select.
 c.pack()
 
 c_label = Label(root, text=var.get())
